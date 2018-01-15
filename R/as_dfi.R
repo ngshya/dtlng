0ccf21cfec1a37157fdbf81fd631869f450ff5df
ngshya <ngshya@gmail.com>
2018-01-15 17:40:08 +0100

    Updated asDfi() by adding a new parameter: str_name.

--- R/as_dfi.R
+++ R/as_dfi.R
@@ -1,5 +1,14 @@
+#' @title asDfi
+#' @description Transform a dataframe in a dfi.
+#'
+#' @param x A dataframe.
+#' @param str_name (optional) The name of the new dfi.
+#'
+#' @return A dfi with the desired name.
+#'
 #' @export
 
-asDfi <- function(x){
-  base::return(dfi$new(dataframe = x))
+asDfi <- function(x, str_name){
+  if(base::missing(str_name)){str_name <- ""}
+  base::return(dfi$new(dataframe = x, name = str_name))
 }
